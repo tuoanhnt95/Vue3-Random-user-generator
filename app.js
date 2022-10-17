63abbb2220a56df2d01fe4c544a765a1029a6d26
Oanh Nguyen <ONguyen@HTMJPN.local>
2022-10-17 16:10:14 +0900

    feat: Add basic user's info

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const app = Vue.createApp({
    data() {
       return {
-         firstName: 'John'
+         firstName: 'John',
+         lastName: 'Doe',
+         email: 'john@gmail.com',
+         gender: 'male',
+         picture: 'https://randomuser.me/api/portraits/men/10.jpg'
       }
    }
 })
